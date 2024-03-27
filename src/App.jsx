9db4b2a0480cb15c0440a64e04e1848851e2696d
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-03-27 17:38:46 +0545

    data fetching using createAsyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import CakeView from "./features/cake/CakeView";
+import IcecreamView from "./features/icecream/IcecreamView";
+import UsersView from "./features/users/UsersView";
 
 function App() {
   return (
     <div id="appPageId">
       <CakeView />
+      <IcecreamView />
+      <UsersView />
     </div>
     //
   );
